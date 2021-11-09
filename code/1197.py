5afeef29e79e7539f024540a105810ba93fdd72d
Where are you going? <49277976+jgyy4775@users.noreply.github.com>
2021-11-09 14:33:07 +0900

    Create 1197.py

---
+++ code/1197.py
@@ -0,0 +1,31 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [0] * (v+1)
+edges = []
+result = 0
+for i in range(1, v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b)) ## 비용순 정렬
+
+edges.sort()
+for edge in edges:
+    c, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += c
+print(result)
