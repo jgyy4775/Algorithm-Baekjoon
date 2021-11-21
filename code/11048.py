b84fa540dffd91eaa6e37a1eea191530b454d641
Where are you going? <49277976+jgyy4775@users.noreply.github.com>
2021-11-21 23:06:13 +0900

    Create 11048.py

---
+++ code/11048.py
@@ -0,0 +1,12 @@
+import sys
+n, m = map(int, input().split())
+graph = []
+dp =[[0]*(m+1) for _ in range(n+1)]
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+graph[i-1][j-1]
+
+print(dp[-1][-1])
