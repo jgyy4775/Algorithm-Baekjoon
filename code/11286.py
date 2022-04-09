af659014d5f7bc83c0ab3d7a1bd85ffb11ca34c4
Ga Young <49277976+jgyy4775@users.noreply.github.com>
2022-04-09 20:27:41 +0900

    Create 11286.py

---
+++ code/11286.py
@@ -0,0 +1,10 @@
+import sys, heapq
+n=int(sys.stdin.readline())
+heap=[]
+for _ in range(n):
+    x=int(sys.stdin.readline())
+    if x==0:
+        if len(heap)==0: print(0)
+        else: print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, [abs(x),x])
